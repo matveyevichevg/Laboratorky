1ebd97dd8864dee40500e9f284eabd962b3b6df4
matveyevichevg <matveyevichevg@gmail.com>
2021-09-23 16:36:52 +0700

    1 lab

--- Lab1/2.py
+++ Lab1/2.py
@@ -1,5 +1,25 @@
-u1 = input('Введи первый угол: ')
-u2 = input('Введи второй угол: ')
+def ex(f):
+    try:
+        if float(f) > 0:
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
+while True:
+    u1 = input('Введи первый угол: ' )
+    if ex(u1):
+        a=float(u1)
+        break
+    else:
+        print('Ошибка, Введите положительное число')
+while True:
+    u2 = input('Введи второй угол: ' )
+    if ex(u2):
+        a=float(u2)
+        break
+    else:
+        print('Ошибка, Введите положительное число')
 if int(u1) + int(u2) == 180:
     print('Ошибка')
 elif int(u1) == 90 or int(u2) == 90:
