e7f758f76a7c7d474f71a5ac63bc31226232c71f
matveyevichevg <matveyevichevg@gmail.com>
2021-09-23 16:40:53 +0700

    1 lab

--- Lab1/3.py
+++ Lab1/3.py
@@ -1,3 +1,18 @@
-F = input('Введите градусы по фаренгейту: ')
+def ex(f):
+    try:
+        if int(f):
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
+while True:
+    F = input('Введите градусы по фаренгейту: ')
+    if ex(F):
+        F=float(F)
+        break
+    else:
+        print('Ошибка, ведите число')
+
 C = (5/9)*(int(F)-32)
 print('По цельсию равно ', int(C))
