b2d503597f553201987c0d8c3af1c6435a2d8d1b
matveyevichevg <matveyevichevg@gmail.com>
2021-09-23 15:56:15 +0700

    1 lab

--- Lab1/1.py
+++ Lab1/1.py
@@ -1,5 +1,27 @@
-import math as m
-a = float(input('Веедите катет a: ' ))
-b = float(input('Веедите катет b: '))
-gip =m.sqrt (a**2 + b**2)
+import math
+def ex(f):
+    try:
+        if float(f) > 0:
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
+while True:
+    a = input('Веедите катет a: ' )
+    if ex(a):
+        a=float(a)
+        break
+    else:
+        print('Ошибка, Введите положительное число')
+while True:
+    b = input('Веедите катет b: ' )
+    if ex(b):
+        b=float(b)
+        break
+    else:
+        print('Ошибка, Введите положительное число')
+gip = math.sqrt ((a)**2 + (b)**2)
 print('Гипотенуза равна: {0:.1f} '.format(gip))
+
+
