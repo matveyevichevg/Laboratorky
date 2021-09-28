61418ed5d3515ac173bff3051342d4851ceddb56
matveyevichevg <matveyevichevg@gmail.com>
2021-09-28 17:18:53 +0700

    1 lab

--- Lab1/1.py
+++ Lab1/1.py
@@ -1,6 +1,7 @@
 import math
 
-def ex(f):
+# функция проверки значения, принимает переменную неопределенного типа, возвращает True если это число и оно больше нуля, False во всех остальных случаях
+def isPositiveNumber():
     try:
         if float(f) > 0:
             return True
@@ -9,21 +10,25 @@ def ex(f):
     except ValueError:
         return False
 
-while True:
-    a = input('Веедите катет a: ' )
-    if ex(a):
-        a = float(a)
-        break
-    else:
-        print('Ошибка, Введите положительное число')
+# функция ввода значения катетов, принимает переменную а и b, возвращает их если они прошли проверку isPositiveNumber
+def getPositiveNumber():
+    while True:
+        a = input('Веедите катет a: ' )
+        if isPositiveNumber(a):
+            a = float(a)
+            return a
+        else:
+            print('Ошибка, Введите положительное число')
+    while True:
+        b = input('Веедите катет b: ' )
+        if isPositiveNumber(b):
+            b = float(b)
+            return b
+        else:
+            print('Ошибка, Введите положительное число')
 
-while True:
-    b = input('Веедите катет b: ' )
-    if ex(b):
-        b = float(b)
-        break
-    else:
-        print('Ошибка, Введите положительное число')
+a=getPositiveNumber()
+b=getPositiveNumber()
 
 gip = math.sqrt(a**2 + b**2)
 print('Гипотенуза равна: {0:.1f} '.format(gip))
