ce6a9bea06f16dfbabb3c943dc4938e9ee527bb3
matveyevichevg <matveyevichevg@gmail.com>
2021-09-29 13:53:13 +0700

    В модуль добавлены функция проверки на число и функция ввода числа

--- Lab1/CheckInput.py
+++ Lab1/CheckInput.py
@@ -23,3 +23,25 @@ def getPositiveNumber(message):
             return n
         else:
             print('Ошибка, Введитe положительное число')
+
+# #Функция проверяет входное значение на число
+# возвращает True если это число, False в случае если это строка
+def isNumber(num):
+    try:
+        if float(num):
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
+
+#Функция ввода значения и провекри его на число
+def getNumber(message):
+    while True:
+        n = input(message)
+        if isNumber(n):
+            n = float(n)
+            return n
+        else:
+            print('Ошибка, Введитe число')
+
