d4e3d87ab110574ceac1ec0d03944fe9a4a14c76
matveyevichevg <matveyevichevg@gmail.com>
2021-10-19 14:19:12 +0700

    Реализация 3 задачки

---
+++ Lab2/3.py
@@ -0,0 +1,42 @@
+# В одномерном массиве целых чисел заменить максимальное значение минималь-ным.
+import CheckInput
+import numpy as np
+
+def vvod():
+    array = CheckInput.arrayInput_int()
+    return array
+
+#Замена максимального значения на минимальное
+def obrabotka(array):
+    #Поиск индекса максимального значения
+    maxArgument = np.argmax(array)
+    #Поиск индекса минимального значения
+    minArgument = np.argmin(array)
+    #Замена максимального значения - минимальным
+    array[maxArgument] = minArgument
+
+def vyvod(array):
+    print(array)
+
+#Реализация меню
+choise = CheckInput.menu()
+a = []
+while choise == 2 or choise == 3:
+    print('Сначала введи массив(п.1)\n')
+    choise = CheckInput.menu()
+while choise:
+    if choise == 1:
+        a = vvod()
+        print()
+        choise = CheckInput.menu()
+    elif choise == 2:
+        obrabotka(a)
+        print('Массив обработан')
+        print()
+        choise = CheckInput.menu()
+    elif choise == 3:
+        vyvod(a)
+        print()
+        choise = CheckInput.menu()
+    elif choise == 4:
+        quit()
